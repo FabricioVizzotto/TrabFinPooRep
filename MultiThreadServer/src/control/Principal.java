fdb297d21ead1b72f99d28776147145145ede5b8
luhpi <luiza.pinheiro2011@gmail.com>
2018-07-07 20:35:04 -0300

    Cooler ver

--- MultiThreadServer/src/control/Principal.java
+++ MultiThreadServer/src/control/Principal.java
@@ -1,12 +1,13 @@
 package control;
 
-import model.Dados;
 
 public class Principal {
 	
 	public static void main(String[] args) {
 		
 		SocketServidor sserv = new SocketServidor();
+		double alt, peso, imc;
+		String texto;
 		
 		sserv.createServerSocket(55555);
 		sserv.createReader();
@@ -14,8 +15,21 @@ public class Principal {
 
 		while(true){
 			sserv.bindToClient();
-			sserv.receiveData();
+			
+			do {
+				texto = sserv.receiveData();
+			}while(texto.equals(""));
+			
+			alt = Double.parseDouble(texto);
+			peso = Double.parseDouble(sserv.receiveData());
+			imc = calcImc(alt, peso);
+			sserv.sendData(Double.toString(imc));
 		}
 		
+	}
+
+	public static double calcImc(double alt, double peso) {
+		double imc = peso / (Math.pow(2, alt));
+		return imc;
 	}
 }
