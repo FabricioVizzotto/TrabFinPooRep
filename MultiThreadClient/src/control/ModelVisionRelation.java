0f57491b8e49a750d094f35b0108a7ec02915f7b
luhpi <luiza.pinheiro2011@gmail.com>
2018-07-07 18:05:04 -0300

    Append

--- MultiThreadClient/src/control/ModelVisionRelation.java
+++ MultiThreadClient/src/control/ModelVisionRelation.java
@@ -1,4 +1,5 @@
-package control;
+package model;
+
 import java.awt.event.ActionListener;
 import vision.JanelaCliente;
 import model.Dados;
@@ -8,12 +9,14 @@ public class ModelVisionRelation implements ActionListener{
   JanelaCliente t = new JanelaCliente();
   Dados d;
 
-  public ModelVisionRelation(JanelaCliente j){
+  public ModelVisionRelation(JanelaCliente j, Dados d){
     this.j = j;
+    this.d = d;
 
     this.j.getBtnSubmeter().addActionListener(new ActionListener(){
         public void ActionPerformed(ActionEvent arg0){
           d = new Dados(this.t.getTxtAltura.getText(), this.t.getTxtPeso.getText());
+          d.imc = fazImc(d);
         }
     });
 
@@ -25,6 +28,9 @@ public class ModelVisionRelation implements ActionListener{
         }
     });
   }
-  
-  
+
+  public double fazImc(Dados d){
+    imc = d.peso / (Math.pow(2, d.altura));
+    return imc;
+  }
 }
