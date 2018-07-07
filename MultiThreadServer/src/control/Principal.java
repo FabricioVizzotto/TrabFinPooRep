7f548349b1439356fdd69957ab9f363d33224e85
FabricioVizzotto <fabriciovizzotto@ufcspa.edu.br>
2018-07-07 20:06:10 -0300

    Made Some Usefulstuff
    não sei o que dar de nome pra esse commit, mas ele é legal.

---
+++ MultiThreadServer/src/control/Principal.java
@@ -0,0 +1,12 @@
+package control;
+
+import model.Dados;
+
+public class Principal {
+	
+	private SocketServidor sserv;
+	private Dados d;
+	public static void main(String[] args) {
+		
+	}
+}
--- MultiThreadServer/src/control/SocketServidor.java
+++ MultiThreadServer/src/control/SocketServidor.java
@@ -1,9 +1,12 @@
 package control;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class SocketServidor {
 
@@ -14,5 +17,51 @@ public class SocketServidor {
 	private PrintWriter pw;
 	private BufferedReader br;
 	
+	
+	public void bindToClient(){
+		try {
+			cli = serv.accept();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void createWriter(){
+		try {
+			pw = new PrintWriter(cli.getOutputStream());
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void sendData(String data){
+		pw.write(data);
+	}
+	public void createReader(){
+		try {
+			br = new BufferedReader(new InputStreamReader(cli.getInputStream()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public String receiveData(){
+		String dados = null;
+		try {
+			while(br.ready()){
+				dados = br.readLine();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dados;
+				
+	}
 	
 }
