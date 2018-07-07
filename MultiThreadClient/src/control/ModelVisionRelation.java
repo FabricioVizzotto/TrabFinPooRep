c4d9a2f1ef77d2b87b2cbd26ca8dd84e323b22be
luhpi <luiza.pinheiro2011@gmail.com>
2018-07-07 17:21:53 -0300

    ModelVisionRelation added
    New features!

--- MultiThreadClient/src/control/ModelVisionRelation.java
+++ MultiThreadClient/src/control/ModelVisionRelation.java
@@ -1,5 +1,31 @@
-package control;
+package model;
 
-public class ModelVisionRelation {
+import java.awt.event.ActionListener;
+import vision.JanelaCliente;
+import model.Dados;
 
+public class ModelVisionRelation implements ActionListener{
+
+  JanelaCliente t = new JanelaCliente();
+  Dados d;
+
+  public ModelVisionRelation(JanelaCliente j){
+    this.j = j;
+
+    this.j.getBtnSubmeter().addActionListener(new ActionListener(){
+        public void ActionPerformed(ActionEvent arg0){
+          d = new Dados(this.t.getTxtAltura.getText(), this.t.getTxtPeso.getText());
+        }
+    });
+
+    this.j.getBtnLimpar().addActionListener(new ActionListener(){
+        public void ActionPerformed(ActionEvent arg0){
+          this.t.getTxtPeso.setText("");
+          this.t.getTxtAltura.setText("");
+          this.t.getLblIMC.setText("");
+        }
+    });
+  }
+  
+  
 }
