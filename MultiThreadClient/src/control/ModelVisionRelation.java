ce27f706e4bdaaaa07b5b4c813b2861f207b2934
luhpi <luiza.pinheiro2011@gmail.com>
2018-07-07 23:44:48 -0300

    better action

--- MultiThreadClient/src/control/ModelVisionRelation.java
+++ MultiThreadClient/src/control/ModelVisionRelation.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 import vision.JanelaCliente;
 import model.Dados;
 
-public class ModelVisionRelation {
+public class ModelVisionRelation implements ActionListener{
 
 	private JanelaCliente j = new JanelaCliente();
 	private Dados d;
@@ -14,19 +14,22 @@ public class ModelVisionRelation {
 		this.j = j;
 		this.d = d;
 
-		j.getBtnSubmeter().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				d.setAlt(Double.parseDouble(j.getTxtAltura().getText()));
-				d.setPeso(Double.parseDouble(j.getTxtPeso().getText()));
-			}
-		});
+		j.getBtnSubmeter().addActionListener(this);
+		j.getBtnLimpar().addActionListener(this);
+	}
 
-		j.getBtnLimpar().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				j.getTxtPeso().setText("");
-				j.getTxtAltura().setText("");
-				j.getLblIMC().setText("");
-			}
-		});
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getActionCommand().equals("Submeter")) {
+			d.setAlt(Double.parseDouble(j.getTxtAltura().getText()));
+			d.setPeso(Double.parseDouble(j.getTxtPeso().getText()));
+		}
+		if(e.getActionCommand().equals("Limpar")) {
+			j.getTxtPeso().setText("");
+		j.getTxtAltura().setText("");
+		j.getLblIMC().setText("");
+		}
+		
 	}
 }
